4c9a838d0303a640f5a805dad8614e3b500b229c
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2022-12-06 02:16:16 +0500

    Working on notice board

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -187,6 +187,13 @@ class ClassResources(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = models.Manager()
+class NoticeBoard(models.Model):
+    id = models.AutoField(primary_key=True)
+    notice_name = models.TextField()
+    notice_details = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
 
 
 #Creating Django Signals
